18859b048ed1b7c8e8b99cb013beaf5afc714d3d
Quinton Fults <coding.banjo@gmail.com>
2020-05-18 11:44:08 -0700

    code

--- server.js
+++ server.js
@@ -1,9 +1,53 @@
 const http = require('http')
+const { readFile } = require('fs')
+const { promisify } = require('util')
+
+const rf = promisify(readFile)
+
+// const server = http.createServer((req, res) => {
+//   switch (req.url) {
+//     case '/movies':
+//       res.end('Favorite Movies')
+//       break
+//     case '/songs':
+//       res.end('Favorite Songs')
+//       break
+//     case '/foods':
+//       res.end('Favorite Foods')
+//       break
+//     case '/games':
+//       res.end('Favorite Games')
+//       break
+//   }
+// })
+// const server = http.createServer((req, res) => {
+//   const pieces = req.url.split('/')
+//   const op = pieces[1]
+//   const a = parseInt(pieces[2])
+//   const b = parseInt(pieces[3])
+
+//   switch (op) {
+//     case 'add':
+//       res.end(`${a + b}`)
+//       break
+//     case 'subtract':
+//       res.end(`${a - b}`)
+//       break
+//     case 'multiply':
+//       res.end(`${a * b}`)
+//       break
+//     case 'divide':
+//       res.end(`${a / b}`)
+//       break
+//   }
+// })
 
 const server = http.createServer((req, res) => {
-  if (req.url === '/greeting') {
-    res.end('Hello!')
-  }
+  rf('index.html', 'utf8')
+    .then(html => {
+      res.end(html)
+    })
+    .catch(err => console.log(err))
 })
 
 server.listen(3000, () => {
